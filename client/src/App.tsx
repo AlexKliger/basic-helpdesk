90db22309887324b81bc61f37b23ddc4167b9062
Alex <alex.kliger@gmail.com>
2023-09-30 18:29:33 -0400

    vite react initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import HelpForm from './components/HelpForm'
+
+function App() {
+  return (
+    <main>
+      <HelpForm />
+    </main>
+  )
+}
+
+export default App
